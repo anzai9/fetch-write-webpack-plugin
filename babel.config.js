3c628746661e768835fd05e001914d1dd446f677
Chien-An Wang <jiananwang1209@gmail.com>
2020-05-01 15:32:43 +0800

    chore: modify the compile

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,17 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        debug: true,
-        useBuiltIns: 'usage',
-        corejs: 3
+        targets: {
+          node: 10
+        }
       }
     ]
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/proposal-object-rest-spread',
-    '@babel/plugin-transform-regenerator'
+    '@babel/plugin-transform-regenerator',
+    '@babel/plugin-transform-destructuring',
+    '@babel/plugin-transform-runtime'
   ]
 }
